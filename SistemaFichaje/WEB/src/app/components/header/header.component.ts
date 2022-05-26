fe3270150bf8d875056434188fbf09b796e74a9e
Danijg2k <danijg2000@gmail.com>
2022-05-27 00:05:32 +0200

    Estilos header y footer actualizados

--- SistemaFichaje/WEB/src/app/components/header/header.component.ts
+++ SistemaFichaje/WEB/src/app/components/header/header.component.ts
@@ -1,25 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  message: string;
-  editMessage: string;
+  opciones = ['Primer componente', 'Segundo componente', 'Tercer componente'];
+  rutas = ['/uno', '/dos', '/tres'];
+  activeLink = this.opciones[0];
 
-  constructor() {
-    this.message = '';
-    this.editMessage = '';
-  }
+  constructor() {}
 
-  // Con esto se 'suscribe' -> queda a la espera de que customMessage cambie en el helper, y entonces message cambiará aqui.
-  ngOnInit(): void {
-    // this.helper.customMessage.subscribe((msg) => (this.message = msg));
-  }
-
-  // changeMessage() {
-  //   this.helper.changeMessage(this.editMessage.toUpperCase());
-  // }
+  ngOnInit(): void {}
 }
