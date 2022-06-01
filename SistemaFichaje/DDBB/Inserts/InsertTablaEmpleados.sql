2b9b763362d5df573dc802d4848a172ff8baa270
Danijg2k <danijg2000@gmail.com>
2022-06-01 20:10:00 +0200

    Registros DDBB actualizados. Tabla multiplos 5

--- SistemaFichaje/DDBB/Inserts/InsertTablaEmpleados.sql
+++ SistemaFichaje/DDBB/Inserts/InsertTablaEmpleados.sql
@@ -1,7 +1,13 @@
 USE Fichaje;
 
-INSERT INTO Empleado VALUES('Daniel Jiménez Gutiérrez',21,'Calle Roma','Programador Java','11222333A','djimenezg@gmail.com','986c78be53210aeae4ae9a1bf4c0a26e44f7d0af24e2f4cee7d6289064650738',1);
-INSERT INTO Empleado VALUES('Mario Ezquerro Castillo',31,'Calle Velázquez','QA Tester','55222443F','mezquerroc@gmail.com','93dbf6365e4f609a767e1a0d7e9de3b3e31deece8ffd2d282a149dd089ffeb05',0);
-INSERT INTO Empleado VALUES('Carmen López Santos',25,'Avenida Paraíso','Full Stack Developer','88555444A','clopezs@gmail.com','86aa93a27ae3ed03c0f7a0cc60f2ad635bf1547c8cc4843fafed121f82b13395',0);
-INSERT INTO Empleado VALUES('Isabel Navarro Gómez',19,'Avenida Madrid','Back End Developer','77444222A','inavarrog@gmail.com','47e209f5fa94e1144e68f954397a567af9cd7218e55d0b08e02b970a90fba007',0);
-INSERT INTO Empleado VALUES('Jorge Álvarez Gil',20,'Calle Picasso','Ingeniero DevOps','22333111A','jalvarezg@gmail.com','2ffbdd3d6745887df8c2b4115b5c9aec9e90391285fe369069306b8036c78a33',0);
+INSERT INTO Empleado VALUES('Daniel Jiménez Gutiérrez',21,'Calahorra','Programador Java','11222333A','djimenezg@gmail.com','986c78be53210aeae4ae9a1bf4c0a26e44f7d0af24e2f4cee7d6289064650738',1);
+INSERT INTO Empleado VALUES('Mario Ezquerro Castillo',31,'Pradejón','QA Tester','55222443F','mezquerroc@gmail.com','93dbf6365e4f609a767e1a0d7e9de3b3e31deece8ffd2d282a149dd089ffeb05',0);
+INSERT INTO Empleado VALUES('Carmen López Santos',25,'Logroño','Full Stack Developer','88555444A','clopezs@gmail.com','86aa93a27ae3ed03c0f7a0cc60f2ad635bf1547c8cc4843fafed121f82b13395',0);
+INSERT INTO Empleado VALUES('Isabel Navarro Gómez',19,'Zaragoza','Back End Developer','77444222A','inavarrog@gmail.com','47e209f5fa94e1144e68f954397a567af9cd7218e55d0b08e02b970a90fba007',0);
+INSERT INTO Empleado VALUES('Jorge Álvarez Gil',20,'Pamplona','Ingeniero DevOps','22333111A','jalvarezg@gmail.com','2ffbdd3d6745887df8c2b4115b5c9aec9e90391285fe369069306b8036c78a33',0);
+
+INSERT INTO Empleado VALUES('Arturo Fuentes Llosa',28,'Logroño','Back End Developer','57897642R','afuentesl@gmail.com','bd45e63dcf22d50beeaf3d0802dda3cd391158b936c23c80ef14a867b826ef28',0);
+INSERT INTO Empleado VALUES('Isabel García Pérez',24,'Zaragoza','Ingeniera de sistemas','33556671G','igarciap@gmail.com','a5ebcbe2893f39a4ef267a463e0f73aa54d1e0af203f3c3c243a3fa5723e007e',0);
+INSERT INTO Empleado VALUES('José Mendoza Gómez',34,'San Adrián','Programador Java','67876943L','jmendozag@gmail.com','958b74fb61bd5bfe1b91563bb4a37b2f2efd7acee886588480ac0e053fe76b94',0);
+INSERT INTO Empleado VALUES('Marta Iturre Fuertes',23,'Pamplona','Diseñadora Web','70215621K','miturrefl@gmail.com','01f7b00724f40a6e34c0614456498168ebeb1dd6946aa1eac8936ca7c96da31b',0);
+INSERT INTO Empleado VALUES('Laura López Fernández',30,'Calahorra','Project Manager','14092123W','llopezf@gmail.com','84e2f14696be06d7d1fdae644346bb8bba7318f8bd038259c860fa62165af5f3',0);
--- SistemaFichaje/WEB/src/app/components/calendario/calendario.component.ts
+++ SistemaFichaje/WEB/src/app/components/calendario/calendario.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
-  styleUrls: ['./calendario.component.css']
+  styleUrls: ['./calendario.component.css'],
 })
 export class CalendarioComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
